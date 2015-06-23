7d07457fbd2c7a7a3f3b1ffc54ca0106a0db600d
Andres Berrios <andres.berrios.j@gmail.com>
2015-06-23 20:52:59 -0300

    Allow admins to edit any campaign

--- db/fixtures/users.rb
+++ db/fixtures/users.rb
@@ -1,4 +1,6 @@
 
 User.seed_once(:id, :email,
-  {id: 1, email: 'andres.berrios.j@gmail.com', password: 'holahola', role: 'admin'}
+  {id: 1, email: 'andres.berrios.j@gmail.com', password: 'holahola', role: 'admin'},
+  {id: 2, email: 'rodolfo.faundez@smarket.cl', password: 'holahola', role: 'admin'},
+  {id: 3, email: 'felipe.urbina@smarket.cl', password: 'holahola', role: 'admin'}
 )
